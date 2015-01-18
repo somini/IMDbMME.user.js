6c0659775356d862fc08c9254998242d1e81f218
somini <somini29@yandex.com>
2015-01-18 23:14:54 +0000

    Initial commit
    This is a copy of the original script

---
+++ IMDbMME.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name          IMDb "My Movies" enhancer
+// @description   Emphasize the links for movies on your "My Movies" & "Vote History" list
+// @namespace     http://www.flickr.com/photos/ricardo_ferreira/2502798105/
+// @include       http://*.imdb.com/*
+// @match         http://*.imdb.com/*
+// @exclude       http://i.imdb.com/*
+// @exclude       http://*imdb.com/images/*
+// @exclude       http://*imdb.com/list/export*
+// @version       1.35
+// @grant         
+// ==/UserScript==
